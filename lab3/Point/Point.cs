71ff9e42a49235e4a8d092f5e9cba28d5c576b89
Zakhar <zzz.krav@gmail.com>
2022-11-03 03:08:59 +0200

    fourth commit

---
+++ lab3/Point/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    class Point
+    {
+        private readonly int _x;
+
+        private readonly int _y;
+
+        private readonly string _name;
+        public int X => _x;
+        public int Y => _y;
+        public string Name => _name;
+
+        public Point(int x, int y, string name)
+        {
+            this._x = x;
+            this._y = y;
+            this._name = name;
+        }
+    }
+}
